ccf84f42a84081b2d203b1728f5462c7461456ba
jose-dp <55983469+jose-dp@users.noreply.github.com>
2019-10-29 16:07:03 +0100

    Update ClaseExamen.java

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -4,19 +4,26 @@ public class ClaseExamen {
 	
 	public float metodoAProbar(int a, int b) {
 		
+		// nodo 1
 		float result = 0;
 		
+		// nodo 2
 		while( a > b) {
+			// nodo 3
 			a--;
 		}
 		
+		// nodo 4
 		if (a <= b) {
+			// nodo 5
 			result = 0;
 		}
 		else {
+			// nodo 6
 			result = b;
 		}
 		
+		// nodo 7
 		return result;
 	}
 
