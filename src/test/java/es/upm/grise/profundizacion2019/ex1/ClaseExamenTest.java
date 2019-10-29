4d57542728a19e222e4e8bb49774d4ad75315fa8
jose-dp <55983469+jose-dp@users.noreply.github.com>
2019-10-29 16:07:53 +0100

    Update ClaseExamenTest.java

--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -7,8 +7,21 @@ import org.junit.Test;
 public class ClaseExamenTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// C1
+	public void test1() {
+		ClaseExamen o = new ClaseExamen();
+		assertEquals(0, o.metodoAProbar(1, 1), 0);
 	}
+	
+	@Test
+	// C2
+	public void test2() {
+		ClaseExamen o = new ClaseExamen();
+		assertEquals(0, o.metodoAProbar(2, 1), 0);
+	}
+	
+	// C3 - No se puede recorrer, ya que una vez pasado el while,
+	// la a siempre tendrá el mismo valor que la b, por lo tanto,
+	// la evaluación del if siempre dará verdadero
 
 }
